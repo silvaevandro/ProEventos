f9d8f86f6d4363a2bf60eba7098c516268a75337
Evandro Silva <silva_evandro@outlook.com.br>
2021-12-06 23:05:48 -0300

    Tela de seleção dos eventos

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
@@ -23,6 +24,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any ;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  mostrarImagem: boolean = false;
+  private _filtroLista: string = "";
+
+  public get filtroLista() : string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos
+  }
 
   constructor(private http:HttpClient) { }
 
@@ -16,9 +28,20 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  filtrarEventos(filtrar: string): any{
+    filtrar = filtrar.toLocaleLowerCase();
+    return this.eventosFiltrados.filter(
+      (evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrar) !== -1
+    );
+  }
+
+
   public getEventos(): void{
     this.http.get('https://localhost:7272/api/eventos').subscribe(
-      res => this.eventos = res,
+      res => {
+        this.eventos = res
+        this.eventosFiltrados = res
+      },
       error => console.log(error)
     );
   }

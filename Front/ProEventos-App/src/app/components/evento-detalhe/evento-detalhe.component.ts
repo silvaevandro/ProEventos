0f952908274456b5820aee9360e1236093d402d6
Evandro Silva <silva_evandro@outlook.com.br>
2021-12-16 17:01:48 -0300

    Inicio reactive-forms

--- Front/ProEventos-App/src/app/components/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/evento-detalhe/evento-detalhe.component.ts
@@ -1,29 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
   styleUrls: ['./evento-detalhe.component.scss'],
 })
-export class EventoDetalheComponent  implements OnInit {
+export class EventoDetalheComponent implements OnInit {
   form!: FormGroup;
-  constructor() {}
+
+  get f(): any {
+    return this.form.controls;
+  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.validation();
   }
 
+  public resetForm(): void{
+    this.form.reset();
+  }
+
   public validation(): void {
-    this.form = new FormGroup({
-      tema: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
-      local: new FormControl('', Validators.required),
-      dataEvento: new FormControl('', Validators.required),
-      qtdPessoas: new FormControl('', Validators.required),
-      lote: new FormControl('', Validators.required),
-      imagemURL: new FormControl('', Validators.required),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
+    this.form = this.fb.group({
+      tema: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ],
+      ],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', Validators.required],
+      //lote: ['', Validators.required],
+      imagemURL: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 }

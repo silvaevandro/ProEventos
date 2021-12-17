835f27cb31f718b24f4632a5bb9964bdab07e980
Evandro Silva <silva_evandro@outlook.com.br>
2021-12-17 14:37:39 -0300

    Validações no cadastro/perfil usuário no frontend

--- Front/ProEventos-App/src/app/components/user/cadastro/cadastro.component.ts
+++ Front/ProEventos-App/src/app/components/user/cadastro/cadastro.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from '@app/Helpers/ValidatorsField';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
+
+  public validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmacaoSenha'),
+    };
+
+    this.form = this.fb.group(
+      {
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        usuario: ['', Validators.required],
+        senha: ['', Validators.required],
+        confirmacaoSenha: ['', Validators.required],
+      },
+      formOptions
+    );
+  }
 }
--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from '@app/Helpers/ValidatorsField';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss'],
 })
-export class PerfilComponent {
-  constructor() {}
+export class PerfilComponent implements OnInit {
+  form!: FormGroup;
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
+
+  public validation(): void {
+    // const formOptions: AbstractControlOptions = {
+    //   validators: ValidatorField.MustMatch('senha', 'confirmacaoSenha'),
+    // };
+
+    this.form = this.fb.group({
+      titulo: ['', [Validators.required]],
+      primeiroNome: ['', [Validators.required]],
+      ultimoNome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required]],
+      funcao: ['', [Validators.required]],
+      descricao: ['', [Validators.required]],
+      // senha: ['', [Validators.required]],
+      // confirmacaoSenha: ['', [Validators.required]],
+    });
+  }
 }
